fdca719bb41b6aaf5a66960205e48ce62aca8e35
Nicholas Ho <nah1222@yahoo.com>
2020-08-14 15:59:46 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(array)
   new_array = array.split(" ")
-  reversed_array = new_array.each {|x| x.reverse!}
-  return reversed_array.join(" ")
+  new_array.collect {|x| x.reverse!}
+  new_array.join(" ")
 end
